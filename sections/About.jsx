739a188e08798a5707454bd6c6a4e0156464125d
Caleb B <calebabaca@gmail.com>
2023-11-05 20:35:15 -0800

    test run 1

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,22 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { typingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0"/>
+    <motion.div
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: false, amount: 0.25}}
+    >
+
+    </motion.div>
     About section
   </section>
 );
